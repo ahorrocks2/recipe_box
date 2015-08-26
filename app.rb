56b62d2738624f2c392b7e9ef33562f0bbd865d3
Alex Altieri & Alyssa Horrocks <student@epicodus.com>
2015-08-26 14:24:42 -0700

    Allow user to update category for a recipe

--- app.rb
+++ app.rb
@@ -18,13 +18,24 @@ post('/recipes') do
   name = params.fetch("name")
   ingredient = params.fetch("ingredient")
   instruction = params.fetch("instruction")
+  category_id = params.fetch("category_id").to_i()
   recipe = Recipe.create({:name => name, :ingredient => ingredient, :instruction => instruction})
+  recipe.update({:category_ids => category_id})
   redirect('/')
 end
 
 get('/recipes/:id') do
   @recipe = Recipe.find(params.fetch('id'))
+  @categories = Category.all()
   erb(:recipe)
+end
+
+patch('/recipes/:id') do
+  category_id = params.fetch("category_id").to_i()
+  id = params.fetch("id").to_i()
+  @recipe = Recipe.find(id)
+  @recipe.update({:category_ids => category_id})
+  redirect("/recipes/#{id}")
 end
 
 get('/category_form') do
@@ -35,4 +46,9 @@ post('/categories') do
   name = params.fetch("name")
   category = Category.create({:name => name})
   redirect('/')
+end
+
+get('/categories/:id') do
+  @category = Category.find(params.fetch("id").to_i)
+  erb(:category)
 end
